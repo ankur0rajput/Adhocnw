0f733fe0e5830b0fa952e9e4de9be74f2a838b19
Ankur kumar <31175172+ankur0rajput@users.noreply.github.com>
2019-03-19 20:26:59 +0530

    Update Server.py

--- Client to server/Server.py
+++ Client to server/Server.py
@@ -5,7 +5,6 @@ Created on Mon Mar 18 07:47:48 2019
 @author: hp
 """
 import socket
-
 s = socket.socket()
 port = 12345
 
@@ -17,23 +16,29 @@ while True:
 
 # Establish connection with client. 
     c, addr = s.accept()	 
-    #print ('Got connection from', addr) 
-
-# send a thank you message to the client. 
-    c.send('Enter the email')
-    data_1=c.recv(1024)
-    data_1=data_1.decode("utf-8")
+    print ('Got connection from', addr) 
+    data_1=b'Enter the email'
+    c.send(data_1)
+    data_2=c.recv(1024)
+    data_2=data_2.decode("utf-8")
     
-    if data_1=="ankur":
-        c.send('Enter the password')
-        data_2=c.recv(1024)
-        data_2=data_2.decode("utf-8")
-        if data_2=="kumar":
-            c.send("connection established")
+    if data_2=="ankur":
+        data_3=b'Enter the password'
+        #data_3.encode("utf-8")
+        c.send(data_3)
+        
+        #data_69=bytes(data_2, 'utf-8')
+        #c.send(data_69)
+        data_4=c.recv(1024)
+        data_4=data_4.decode("utf-8")
+        if data_4=="kumar":
+            data_5=b'connection established'
+            #data_5.encode("utf-8")
+            c.send(data_5)
             c.close()
         else:
-            c.send("not valid")
+            c.send(b'not valid')
             c.close()
     else:
-        c.send("not valid")
+        c.send(b'not valid')
         c.close() 
