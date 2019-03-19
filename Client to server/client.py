5a957de11492048cc209768585f743f00c117b7c
Ankur kumar <31175172+ankur0rajput@users.noreply.github.com>
2019-03-19 18:44:22 +0530

    Update client.py

--- Client to server/client.py
+++ Client to server/client.py
@@ -5,15 +5,25 @@ Created on Mon Mar 18 08:00:37 2019
 @author: hp
 """
 
-import socket
+import socket			 
 
-client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('0.0.0.0', 8080))
+# Create a socket object 
+s = socket.socket()		 
 
-client.send("I am a client \n")
+# Define the port on which you want to connect 
+port = 12345				
 
-from_server=client.recv(4096)
+# connect to the server on local computer 
+s.connect(('127.0.0.1', port))
 
-client.close()
+print (s.recv(1024))
+data_1=input()
+str(data_1).encode("utf-8")
+s.send(data_1)
 
-print(from_server)
+print (s.recv(1024))
+data_2=input()
+str(data_2).encode("utf-8")
+s.send(data_2)
+
+s.close()
